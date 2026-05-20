7e4bd80aa7afb53f4d33bf38bc2eff32fde66ebf
Vijay Chauhan <vijaychauhan4632@gmail.com>
2026-05-20 20:14:07 +0530

    Database Fixed

--- server/index.js
+++ server/index.js
@@ -116,7 +116,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5500;
 
 // Start database then express app
 connectDB().then(() => {
